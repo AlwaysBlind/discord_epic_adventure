f1c32727d3d4969f68e711492a0839625db18d52
Ulf Fäldt Reifenberg <u.faldt.reifenberg@navblue.aero>
2021-02-14 12:20:04 +0100

    Add enum for states

--- game.py
+++ game.py
@@ -1,71 +1,87 @@
 
+from enum import Enum
+
+
 class Game():
     active = False
     cat = "🐅"
     hero = "🤠"
-    skull = "☠" 
+    skull = "☠"
     MAX_TURNS_HIDDEN = 4
-    n = 15
+    n = 25
     inputs = []
 
     def start(self):
-        self.state = "hunt"
+        self.state = State.HUNT
         self.active = True
         self.field = [" "]*self.n
         self.cat_pos = self.n - 1
-        self.hero_pos = 0
+        self.hero_pos = 7
         self.hero = "🤠"
         self.turns_hidden = 0
+
     def hide(self):
-        self.inputs.append("hide")
+        self.inputs.append(State.HIDE)
 
     def update(self):
-        if self.active == False:
+        if not self.active:
             return
         for inp in self.inputs:
-            if inp == "hide" and self.turns_hidden < self.MAX_TURNS_HIDDEN:
-                self.state = "hide"
+            if inp == State.HIDE and self.turns_hidden < self.MAX_TURNS_HIDDEN:
+                self.state = State.HIDE
+
         self.inputs.clear()
-        if self.state == "hide":
+        if self.state == State.HIDE:
             if self.turns_hidden < self.MAX_TURNS_HIDDEN:
                 self.turns_hidden += 1
             else:
-                self.state = "hunt"
+                self.state = State.HUNT
         self.cat_pos -= 1
-        if self.cat_pos == self.hero_pos and not self.state == "hide" :
+        if self.cat_pos == self.hero_pos and not self.state == State.HIDE:
             self.hero = self.skull
-            self.state = "dead"
+            self.state = State.DEAD
             self.active = False
         if self.cat_pos < 0:
-            self.state = "victory"
+            self.state = State.VICTORY
             self.active = False
 
     def draw(self):
         header = "\n"
         header2 = "\n"
         msg = ""
-        if self.state == "hunt":
-            header = "The fierce cat is coming closer. What are you going to do?"
+        if self.state == State.HUNT:
+            header = "The fierce cat is coming closer." \
+                "What are you going to do?"
             header2 = "Feed him, hide or use brutal violence (gun)"
-        if self.state == "dead":
+        if self.state == State.DEAD:
             header = "Too bad. You died"
-        if self.state == "hide":
+        if self.state == State.HIDE:
             header = "The cat is coming closer. What are you going to do?"
             header2 = "You hide. But for how long?"
-        if self.state == "victory":
+        if self.state == State.VICTORY:
             header = "The cat has run past you"
-            header2 = "You survive, but what have you gained?"
+            header2 = "You survive. But what have you gained?"
 
         current_field = self.field.copy()
         if (self.cat_pos >= 0):
             current_field[self.cat_pos] = self.cat
-        if self.state == "hide":
-            if self.cat_pos != self.hero_pos:
-                current_field[self.hero_pos] = "🌴"
+
+        if self.state == State.HIDE and self.cat_pos != self.hero_pos:
+            current_field[self.hero_pos] = "🌴"
         else:
             current_field[self.hero_pos] = self.hero
+
         msg = "".join(current_field)
         return f'{header}\n{header2}\n{msg}'
+
+
+class State(Enum):
+    HUNT = 0,
+    HIDE = 1,
+    VICTORY = 2,
+    RUN = 3,
+    DEAD = 4,
+
 
 # g = Game()
 
