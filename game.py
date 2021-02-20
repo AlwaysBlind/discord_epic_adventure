d4c7c806500291326b88ca45e318c02fd16a16a4
Ulf Fäldt Reifenberg <u.faldt.reifenberg@navblue.aero>
2021-02-20 20:05:12 +0100

    Rework states

--- game.py
+++ game.py
@@ -6,20 +6,34 @@ class Game():
     active = False
     skull = "☠️"
     MAX_TURNS_HIDDEN = 4
-    n = 25
+    n = 15
     inputs = []
+    MEAT_KEY = 'meat'
 
     def start(self):
-        self.characters = []
+        self.characters = dict()
         self.hero = character.Hero(7)
         self.cat = character.Tiger(self.n-1)
-        self.meat = None
         self.state = State.HUNT
         self.active = True
-        self.field = self.get_game_field()
+        self.gamefield = self.get_game_field()
         self.turns_hidden = 0
-        self.characters.append(self.hero)
-        self.characters.append(self.cat)
+        self.characters["hero"] = self.hero
+        self.characters["cat"] = self.cat
+        self.header = "\n"
+        self.header2 = "\n"
+        self.state_dict = {State.HUNT: self.handle_state_HUNT,
+                           State.HIDE: self.handle_state_HIDE,
+                           State.VICTORY_HIDE: self.handle_state_VICTORY_HIDE,
+                           State.RUN: self.handle_state_RUN,
+                           State.DEAD: self.handle_state_DEAD,
+                           State.VICTORY_RUN: self.handle_state_VICTORY_RUN,
+                           State.GUN: self.handle_state_GUN,
+                           State.FEED: self.handle_state_FEED,
+                           State.WELLFED: self.handle_state_WELLFED,
+                           }
+        self.handle_state_HUNT()
+        self.update_game_field()
 
     def get_game_field(self):
         field = [" "]*self.n
@@ -42,32 +56,93 @@ class Game():
     def update(self):
         if not self.active:
             return
+        self.gamefield = self.get_game_field()
         self.handle_input()
 
-        if self.state == State.HIDE:
-            if self.turns_hidden < self.MAX_TURNS_HIDDEN:
-                self.turns_hidden += 1
-            else:
-                self.state = State.HUNT
-        for char in self.characters:
-            char.update()
+        for char in self.characters.values():
+            if char is not None:
+                char.update()
 
-        if self.meat and self.meat.pos == self.cat.pos:
-            self.state = State.WELLFED
-            self.meat = None
-            self.active = False
-            return
+        self.handle_state()
+        self.update_game_field()
 
-        if self.cat.pos == self.hero.pos and not self.state == State.HIDE:
-            self.hero.image = self.skull
-            self.state = State.DEAD
-            self.active = False
+    def update_game_field(self):
+        sorted_chars = sorted(self.characters.values(),
+                              key=lambda x: x.priority, reverse=True)
+        for char in sorted_chars:
+            if char is not None:
+                if char.pos >= 0:
+                    self.gamefield[char.pos] = char.image
+
+    def handle_state_VICTORY_RUN(self):
+        self.header = "You outran the cat"
+        self.header2 = "You survive. Will you always outrun your fears?"
+
+    def handle_state_VICTORY_HIDE(self):
+        self.header = "The cat has run past you"
+        self.header2 = "You survive. But a coward you remain"
+
+    def handle_state_RUN(self):
+        self.header = "The cat is still running towards you"
+        self.header2 = "You try to outrun it"
+
+    def handle_state_HIDE(self):
+        if self.turns_hidden < self.MAX_TURNS_HIDDEN:
+            self.turns_hidden += 1
+            self.hero.image = self.hero.base_image
+
+        else:
+            self.header = "Your disguise breaks."
+            self.header2 = "You are now visible"
+
+        self.header = "The cat is coming closer."
+        self.header2 = "You hide. But for how long?"
+        self.hero.image = "🌴"
+
+    def handle_state_DEAD(self):
+        self.header = "The cat ate you"
+        self.header2 = "You are now dead. Too bad"
+
+    def handle_state_WELLFED(self):
+        self.header = "The cat eats the meat."
+        self.header2 = "It is now your friend forever"
+        self.gamefield = ["💖" for x in self.gamefield]
+
+    def handle_state_GUN(self):
+        self.header = "You shot the cat. It stops breathing"
+        self.header2 = "The majestic animal is dead. Why did you do this?"
+        self.cat.image = "️💀"
+
+    def handle_state(self):
+        if self.cat.pos == self.hero.pos:
+            if not (self.state == State.HIDE and
+                    self.turns_hidden >= self.MAX_TURNS_HIDDEN):
+                self.hero.image = self.skull
+                self.state = State.DEAD
+                self.active = False
         if self.cat.pos < 0:
             self.state = State.VICTORY_HIDE
             self.active = False
         if self.hero.pos < 0:
             self.state = State.VICTORY_RUN
             self.active = False
+        self.state_dict[self.state]()
+
+    def handle_state_FEED(self):
+        meat = self.characters.get(self.MEAT_KEY)
+        if meat is not None and meat.pos == self.cat.pos:
+            self.state = State.WELLFED
+            self.characters.pop(self.MEAT_KEY)
+            self.active = False
+            self.handle_state()
+            return
+        self.header = "The fierce cat is coming closer."
+        self.header2 = "You lay out a chunk of meat."
+
+    def handle_state_HUNT(self):
+        self.header = "The fierce cat is coming closer." \
+            "What are you going to do? You can only choose one action"
+        self.header2 = "Feed him, hide or use brutal violence (gun)"
 
     def handle_input(self):
         if self.hero.number_of_actions > 0:
@@ -78,59 +153,19 @@ class Game():
                 self.state = State.HIDE
             elif inp == State.RUN:
                 self.hero.speed = 1
+                self.state = State.RUN
             elif inp == State.GUN:
                 self.state = State.GUN
                 self.active = False
             elif inp == State.FEED:
-                self.meat = character.Meat(self.hero.pos+1)
+                self.characters[self.MEAT_KEY] = character.Meat(self.hero.pos+1)
+                self.state = State.FEED
             self.hero.number_of_actions += 1
             self.inputs.clear()
 
     def draw(self):
-        header = "\n"
-        header2 = "\n"
-        msg = ""
-        if self.state == State.HUNT:
-            header = "The fierce cat is coming closer." \
-                "What are you going to do? Choose carefully"
-            header2 = "Feed him, hide or use brutal violence (gun)"
-        if self.state == State.GUN:
-            header = "You shot the cat. It stops breathing"
-            header2 = "The majestic animal is dead. Why did you do this?"
-            self.cat.image = "️💀"
-        if self.state == State.WELLFED:
-            header = "The cat eats the meat."
-            header2 = "It is now your friend forever"
-        if self.state == State.DEAD:
-            header = "The cat ate you"
-            header2 = "You are now dead. Too bad"
-        if self.state == State.HIDE:
-            header = "The cat is coming closer. What are you going to do?"
-            header2 = "You hide. But for how long?"
-        if self.state == State.VICTORY_HIDE:
-            header = "The cat has run past you"
-            header2 = "You survive. But a coward you remain"
-        if self.state == State.VICTORY_RUN:
-            header = "You outran the cat"
-            header2 = "You survive. Will you always outrun your fears?"
-
-        current_field = self.field.copy()
-        if self.state == State.WELLFED:
-            current_field = ["💖" for x in current_field]
-        if self.meat:
-            current_field[self.meat.pos] = self.meat.image
-
-        if self.state == State.HIDE:
-            if self.cat.pos != self.hero.pos:
-                current_field[self.hero.pos] = "🌴"
-        else:
-            current_field[self.hero.pos] = self.hero.image
-
-        if (self.cat.pos >= 0):
-            current_field[self.cat.pos] = self.cat.image
-
-        msg = "".join(current_field)
-        return f'{header}\n{header2}\n{msg}'
+        msg = "".join(self.gamefield)
+        return f'{self.header}\n{self.header2}\n{msg}'
 
 
 class State(Enum):
